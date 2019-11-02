db55b23f34006fa48898d6dcf6718b2c760514ab
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-11-03 02:32:36 +0530

    [feat]: Aboutme page updated

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,8 @@
 function loaderFunction() {
-    myVar = setTimeout(showLoader, 1000)
- }
+  myVar = setTimeout(showLoader, 1000)
+}
 
 function showLoader() {
-    document.getElementById('loader').style.display = 'none'
-    document.body.style.overflow = 'visible'
+  document.getElementById('loader').style.display = 'none'
+  document.body.style.overflow = 'visible'
 }
